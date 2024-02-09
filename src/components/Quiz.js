44af2f880dc939264e163762447bf85284633ecc
kiruthicksk <skiruthick1234@gmail.com>
2024-02-09 12:00:06 +0530

    commit

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -65,7 +65,7 @@ function Quiz() {
   }, [timer, lock]);
   return (
     <div className="container">
-      <h1>Quiz App</h1>
+      <h1>Welcom to our Quiz App</h1>
       <hr />
       {res ? (
         <></>
